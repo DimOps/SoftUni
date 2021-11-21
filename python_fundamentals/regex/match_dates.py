39ae61e6501b74acb90fb84658bdfa7537ae1f23
Dimitar Dzhapunov <90458809+DimOps@users.noreply.github.com>
2021-11-21 23:00:44 +0200

    Update match_dates.py

--- python_fundamentals/regex/match_dates.py
+++ python_fundamentals/regex/match_dates.py
@@ -3,7 +3,6 @@ import re
 
 string = input()
 
-
 regex = r"(?P<day>\d{2})(?P<separator>[./-])(?P<month>[A-Z][a-z]{2})(?P=separator)(?P<year>\d{4})"
 
 # the regex shows exclusivity with the separator
@@ -17,16 +16,13 @@ for i in range(len(date)):
     sep_date = []
 
     if "/" in date[i]:
-
         sep_date = date[i].split('/')
         print(f'Day: {sep_date[0]}, Month: {sep_date[1]}, Year: {sep_date[2]}')
 
     elif "." in date[i]:
-
         sep_date = date[i].split('.')
         print(f'Day: {sep_date[0]}, Month: {sep_date[1]}, Year: {sep_date[2]}')
 
     elif "-" in date[i]:
-
         sep_date = date[i].split('-')
         print(f'Day: {sep_date[0]}, Month: {sep_date[1]}, Year: {sep_date[2]}')
