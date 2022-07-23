9592522845a7fc6d51841e528fc4c6a95db9be72
dimitar <dd1g17@soton.ac.uk>
2022-07-23 21:37:47 +0300

    some change

--- python_fundamentals/lists_basics/courses.py
+++ python_fundamentals/lists_basics/courses.py
@@ -1,10 +1,10 @@
-n = int(input())
+n = int(2)
 
 list_of_modules = []
 
 for i in range(n):
 
-    word = str(input())
+    word = str('rt')
     list_of_modules.append(word)
 	
 print(list_of_modules)
