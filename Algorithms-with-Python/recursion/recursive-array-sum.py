db4f4dc91d2cffd15cdc8a7753379131f7d326c2
dimitar <dd1g17@soton.ac.uk>
2022-07-25 10:40:27 +0300

    Example of recursion

---
+++ Algorithms-with-Python/recursion/recursive-array-sum.py
@@ -0,0 +1,10 @@
+def calc_sum(vals, index):
+    if index == len(vals) - 1:
+        return vals[index]
+
+    return vals[index] + calc_sum(vals, index + 1)
+
+
+nums = [int(x) for x in input().split()]  # Input: 1 2 3 4
+
+print(calc_sum(nums, 0))  # Output: 10 in the console
