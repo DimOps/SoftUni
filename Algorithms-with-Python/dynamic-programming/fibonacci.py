e5ad7c68e2ea33dc1a2bc945127cc932c14e1597
dimitar <90458809+dimops@users.noreply.github.com>
2022-07-31 16:17:28 +0300

    Estimates Fibonacci sequence

---
+++ Algorithms-with-Python/dynamic-programming/fibonacci.py
@@ -0,0 +1,13 @@
+def calc_fib(n, cache):
+    if n in cache:
+        return cache[n]
+    if n <= 2:
+        return 1
+
+    result = calc_fib(n - 1, cache) + calc_fib(n - 2, cache)
+    cache[n] = result  # provides linear complexity
+    return result
+
+n = int(input())
+
+print(calc_fib(n, {}))
