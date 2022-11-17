a1e8ae9b357d4486fd064f1d577323eeb90ca79a
dimitar <90458809+dimops@users.noreply.github.com>
2022-11-17 22:06:31 +0200

    Created a class Section in "To do list"

---
+++ object_oriented_programming/classes_and_objects/to_do_list/section.py
@@ -0,0 +1,37 @@
+from object_oriented_programming.classes_and_objects.to_do_list.task import Task
+
+
+class Section:
+    
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, new_task: Task):
+        for task in self.tasks:
+            if task.name == new_task.name:
+                return f'Task is already in the section {self.name}'
+
+        self.tasks.append(new_task)
+        return f'Task {new_task.details()} is added to the section'
+
+    def complete_task(self, task_name: str):
+        for task in self.tasks:
+            if task.name == task_name:
+                task.completed = True
+                return f'"Completed task {task_name}'
+
+        return f'Could not find task with the name {task_name}'
+
+    def clean_section(self):
+        completed_tasks = 0
+        for task in self.tasks:
+            if task.completed:
+                self.tasks.remove(task)
+                completed_tasks += 1
+
+        return f'Cleared {completed_tasks} tasks.'
+
+    def view_section(self):
+        tasks = '\n'.join([task.details() for task in self.tasks])
+        return f'Section {self.name}:\n' + f'{tasks}'
